ffb30b80b2786a893bd126c7660f5330078ea48e
Vector-ops <sumith2827@gmail.com>
2023-10-08 22:34:58 +0530

    build: initial setup

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import authRouter from "./routes/auth.routes";
+dotenv.config();
+
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import expressSession from "express-session";
+import prisma from "./db/prisma";
+import { errorHandler } from "./middleware/allRouteCatch";
+
+const app: Express = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	expressSession({
+		cookie: {
+			maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+		},
+		secret: "a santa at nasa",
+		resave: true,
+		saveUninitialized: true,
+		// @ts-ignore
+		store: new PrismaSessionStore(prisma, {
+			checkPeriod: 2 * 60 * 1000, // 2 mins
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+		}),
+	})
+);
+
+app.use("/auth", authRouter);
+app.use(errorHandler);
+app.listen(PORT, () => {
+	console.log("Connected to server on port: ", PORT);
+});
